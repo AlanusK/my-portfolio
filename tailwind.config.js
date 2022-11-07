64ed818e96f79b421265c785f10168475af6c436
Alanus Kajara <alan@Alanuss-MacBook-Pro.local>
2022-11-07 19:02:17 +0300

    update css configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      black: {
+        dark: '#1c1c1c',
+        default: '#000',
+      },
+      white: {
+        default: '#fff',
+      },
+      primary: {
+        default: '#ff5403',
+      },
+      secondary: {
+        default: '#84a1ff',
+      }
+    },
     extend: {},
   },
   plugins: [],
