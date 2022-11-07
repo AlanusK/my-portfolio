6f5ad6d4bc3fcbbd824fb56d4f42e360997a61a2
Alanus Kajara <alan@Alanuss-MacBook-Pro.local>
2022-11-08 00:16:51 +0300

    implement navigation to Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,58 @@
 // dependencies
 import React from 'react';
 import logo from '../../resources/images/logo.png';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Header = () => {
     return (
         <div className='h-20 flex items-center justify-between'>
-            <div>
+            <Link to='/'>
                 <img src={logo} width='50px' alt='logo' />
-            </div>
-            <nav className='w-80'>
-                <ul class='flex items-center justify-between font-bold text-xl'>
-                    <li>About Me</li>
-                    <li>projects</li>
-                    <li>Skills</li>
-                    <li>Contact</li>
+            </Link>
+
+            <nav className='w-96'>
+                <ul className='flex items-center justify-between font-bold text-xl'>
+                    <li>
+                        <NavLink
+                            to='/' 
+                            end
+                            style={({ isActive }) => ({ color: isActive ? '#ff5403' : '#fff' })}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/project'
+                            style={({ isActive }) => ({ color: isActive ? '#ff5403' : '#fff' })}
+                        >
+                            Projects
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/skills'
+                            style={({ isActive }) => ({ color: isActive ? '#ff5403' : '#fff' })}
+                        >
+                            Skills
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/about'
+                            style={({ isActive }) => ({ color: isActive ? '#ff5403' : '#fff' })}
+                        >
+                            About Me
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/contact'
+                            style={({ isActive }) => ({ color: isActive ? '#ff5403' : '#fff' })}
+                        >
+                            Contact
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </div>
