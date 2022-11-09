0a1a5223e17641e4489d6c889d8d2d66f9e1e09e
Alanus Kajara <alan@Alanuss-MacBook-Pro.local>
2022-11-09 13:14:49 +0300

    make home page responsive to all devices

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,27 +7,29 @@ import { Link } from 'react-router-dom';
 export const Home = () => {
     return (
         <div className=''>
-            <div className='flex justify-between'>
-                <div className='flex-auto'>
+            <div className='flex justify-center md:justify-between text-center md:text-left'>
+                <div className='md:flex-auto'>
                     <h5 className='my-6' >Hey!</h5>
                     <h1 className='my-6'>I'm <span className='text-primary-default'>Alanus Kajara</span></h1>
                     <h2 className='my-6 text-secondary-default'>Front-end developer <span className='text-primary-default'>|</span></h2>
                     <p className='my-6 text-lg'>Telecommunications Engineer with a passion for technology.</p>
-                    <p className='my-6 flex items-center text-lg'>
-                        <Link to='/about' className='flex items-center text-primary-default'>Know more<AiOutlineLink style={{ marginLeft: 5, marginRight: 5 }} /></Link>
-                        about me
-                    </p>
+                    <div className='my-6 flex justify-center md:justify-start'>
+                        <p className='flex items-center text-lg'>
+                            <Link to='/about' className='flex items-center text-primary-default'>Know more<AiOutlineLink style={{ marginLeft: 5, marginRight: 5 }} /></Link>
+                            about me
+                        </p>
+                    </div>
                 </div>
-                <div className='flex-auto w-1/3'>
+                <div className='w-1/3 hidden md:block flex-auto'>
                     <img src={profile} width='100%' alt='' />
                 </div>
             </div>
 
             <div>
-                <p className='text-4xl font-bold text-justify'>
+                <p className='text-4xl font-bold text-center md:text-justify'>
                     I help clients to build web and mobile apps for their businesses.
                     <span className='text-lg'>
-                        <Link to='/skills' className='text-primary-default'><AiOutlineLink style={{ marginLeft: 5, marginRight: 5, display: 'inline' }} />View skills.</Link>
+                        <Link to='/skills' className='text-primary-default block md:inline'><AiOutlineLink style={{ marginLeft: 5, marginRight: 5, display: 'inline' }} />View skills.</Link>
                     </span>
                 </p>
             </div>
