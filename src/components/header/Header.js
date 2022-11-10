6868456030cc90ca385c6a932568671776b4521c
Alanus Kajara <alan@Alanuss-MacBook-Pro.local>
2022-11-10 14:11:15 +0300

    improve modal opening and closing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,29 +2,37 @@
 import React, { useState } from 'react';
 import logo from '../../resources/images/logo.png';
 import { Link } from 'react-router-dom';
-import { BiMenu } from 'react-icons/bi';
+import { BiMenu, BiX } from 'react-icons/bi';
 import { Modal } from '../modal';
 import { NavList } from '../nav-list';
 
 export const Header = () => {
-    const [showModal, setShowModal] = useState(true);
+    const [showModal, setShowModal] = useState(false);
     return (
         <div className='h-20 flex items-center justify-between'>
             <div>
                 <Link to='/'><img src={logo} width='50px' alt='logo' /></Link>
             </div>
 
-            <div className='md:hidden'>
-                <button className='bg-primary-default rounded-full p-1'><BiMenu size={40}/></button>
+            <div className='z-10 md:hidden'>
+                {showModal === false ?
+                    <button className='bg-primary-default rounded-full p-1' onClick={() => setShowModal(true)}>
+                        <BiMenu size={40} />
+                    </button>
+                    :
+                    <button className='bg-primary-default rounded-full p-1' onClick={() => setShowModal(false)}>
+                        <BiX size={40} />
+                    </button>
+                }
             </div>
 
             <nav className='w-96 hidden md:inline'>
                 <ul className='flex items-center justify-between font-bold text-xl'>
-                    <NavList /> 
+                    <NavList />
                 </ul>
             </nav>
 
-            {showModal && <Modal setShowModal={setShowModal} /> }
+            {showModal && <Modal setShowModal={setShowModal} />}
         </div>
     )
 }
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,13 +2,13 @@
 import { NavList } from '../nav-list';
 import './Modal.css';
 
-export const Modal = ({ setShowModal }) => {
+export const Modal = (props) => {
     return (
-        <div className='bg-secondary-default w-screen h-screen absolute inset-0 bg-[radial-gradient(#000,#ff5403)]' onClick={() => setShowModal(false)}>
+        <div className='bg-secondary-default w-screen h-screen absolute inset-0 bg-[linear-gradient(to_bottom_left,#000,#ff5403)]'>
             <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                 <div className='text-3xl font-extrabold'>
                     <ul className='flex flex-col items-center justify-between h-[50vh]'>
-                        <NavList />
+                        <NavList setShowModal={props.setShowModal} />
                     </ul>
                 </div>
             </div>
